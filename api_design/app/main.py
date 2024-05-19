6ce3425370e10b014d742a2d349dc9754af1fe6a
Vishwas Gowda <vishwas.g217@gmail.com>
2024-05-19 09:17:39 +0530

    cors

--- api_design/app/main.py
+++ api_design/app/main.py
@@ -3,12 +3,24 @@ from . import models
 from .routers import post, user, auth, vote
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 # creates new tables in the database if they don't exist
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
